ccf63c5305fe747cfc6f401cf88af78a6fee5142
Lazzarotto Maxime <lazzarottomaxime@macbook-pro-de-maxime-2.home>
2021-02-03 23:26:38 +0100

    en burnout d2

---
+++ task5/encrypted.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+sed -e 's/theo1/Wile E. Coyote/g' | sed -e 's/steven1/Daffy Duck/g' | sed -e 's/arnaud1/Porky Pig/g' | sed -e 's/pierre-jean/Marvin the Martian/g' | grep -w "Wile E. Coyote\|Daffy Duck\|Porky Pig\|Marvin the Martian"
+
+
